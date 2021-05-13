f23a896b6c54a72abd6964f0d0c3c72218b7eed0
Galih Aulia Al Hakim <galihalhakim15@gmail.com>
2021-05-13 23:09:28 +0700

    change cofig db

--- server/config/config.js
+++ server/config/config.js
@@ -23,13 +23,13 @@ module.exports = {
   },
   production: {
     url: process.env.DATABASE_URL+`?sslmode=require`,
-    username: process.env.PGNAME,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    dialect: process.env.PGDIALECT,
-    login_field: process.env.LOGIN_FIELD,
+    // username: process.env.PGNAME,
+    // password: process.env.PGPASSWORD,
+    // host: process.env.PGHOST,
+    // port: process.env.PGPORT,
+    // database: process.env.PGDATABASE,
+    // dialect: process.env.PGDIALECT,
+    // login_field: process.env.LOGIN_FIELD,
     timezone: 'Asia/Jakarta'
   },
 }
