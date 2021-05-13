fd5e33a084fd7e13967b0b3b68e17ca82ae77347
Galih Aulia Al Hakim <galihalhakim15@gmail.com>
2021-05-13 23:21:13 +0700

    change config db

--- server/config/config.js
+++ server/config/config.js
@@ -22,15 +22,14 @@ module.exports = {
     timezone: 'Asia/Jakarta'
   },
   production: {
-    // url: process.env.DATABASE_URL+`?sslmode=require`,
-    url: process.env.DATABASE_URL,
-    // username: process.env.PGNAME,
-    // password: process.env.PGPASSWORD,
-    // host: process.env.PGHOST,
-    // port: process.env.PGPORT,
-    // database: process.env.PGDATABASE,
+    url: process.env.DATABASE_URL+`?sslmode=require`,
+    username: process.env.PGNAME,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
     dialect: process.env.DATABASE_DIALECT,
-    // login_field: process.env.LOGIN_FIELD,
+    login_field: process.env.LOGIN_FIELD,
     timezone: 'Asia/Jakarta'
   },
 }
