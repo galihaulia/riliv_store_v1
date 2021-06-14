b424bffa13cc32936e600df46fd1260f5b7ebb0d
Galih Aulia Al Hakim <galihalhakim15@gmail.com>
2021-06-14 12:08:54 +0700

    change purchasing

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,242 +1,227 @@
 require('dotenv').config();
-const asyncHandler = require("../middleware/asyncHandler");
+const asyncHandler = require('../middleware/asyncHandler');
 const moment = require('moment');
 
 const {
-    DB_TABLES: {
-        DB_PURCHASINGS,
-        DB_ITEMS
+  DB_TABLES: { DB_PURCHASINGS, DB_ITEMS },
+} = require('../lib/const');
+
+exports.getAllProducts = asyncHandler(async (req, res, next) => {
+  const products = await DB_ITEMS.findAll({});
+
+  let data = {
+    sumOfProduct: 0,
+    items: [],
+  };
+
+  if (products) {
+    data.sumOfProduct = products.length;
+    data.items = products.map((product) => {
+      return {
+        id: product.id,
+        name: product.name,
+        desc: product.desc,
+        price: product.price,
+      };
+    });
+  }
+
+  res.jsend.success(data);
+});
+
+exports.getProduct = asyncHandler(async (req, res, next) => {
+  const { product_id } = req.query;
+
+  const product = await DB_ITEMS.findOne({
+    where: {
+      id: product_id,
     },
-} = require("../lib/const");
+  });
 
-exports.getAllProducts = asyncHandler(async(req, res, next) => {
-    const products = await DB_ITEMS.findAll({})
+  let data;
+  if (product) {
+    data = {
+      id: product.id,
+      name: product.name,
+      desc: product.desc,
+      price: product.price,
+    };
+  } else {
+    return res.status(400).jsend.error({
+      message: 'Product not found',
+    });
+  }
 
-    let data = {
-        sumOfProduct: 0,
-        items:[]
+  res.jsend.success(data);
+});
+
+exports.createProduct = asyncHandler(async (req, res, next) => {
+  const { product_name, product_desc, product_price } = req.body;
+  const { id, privilegesId } = req.user;
+
+  if (privilegesId == 1) {
+    try {
+      const createProd = await DB_ITEMS.create({
+        usersId: id,
+        name: product_name,
+        desc: product_desc,
+        price: product_price,
+      });
+
+      res.jsend.success({
+        message: 'Product has been created.',
+      });
+    } catch (error) {
+      return res.status(400).jsend.error({
+        message: 'Failed! Product has not been created.',
+      });
     }
+  } else {
+    return res.status(400).jsend.error({
+      message: 'User is not Admin.',
+    });
+  }
+});
 
-    if(products){
-        data.sumOfProduct = products.length
-        data.items = products.map(product => {
-            return {
-                id: product.id,
-                name: product.name,
-                desc: product.desc,
-                price: product.price
-            }
-        })
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+  const { product_id, name, desc, price } = req.body;
+  const { id, privilegesId } = req.user;
+
+  const now = new Date();
+
+  const product = await DB_ITEMS.findOne({
+    where: {
+      id: product_id,
+    },
+  });
+
+  if (privilegesId == 1 && product) {
+    (product.name = name), (product.desc = desc), (product.price = price);
+    product.updatedAt = now;
+    await product.save();
+
+    res.jsend.success({
+      message: 'Product has been updated.',
+    });
+  } else {
+    if (privilegesId != 1) {
+      return res.status(400).jsend.error({
+        message: 'User is not Admin.',
+      });
     }
-
-    res.jsend.success(data)
-})
-
-exports.getProduct = asyncHandler(async(req, res, next) => {
-    const { product_id } = req.query
-
-    const product = await DB_ITEMS.findOne({
-        where:{
-            id: product_id
-        }
-    })
-
-    let data;
-    if(product){
-        data = {
-            id: product.id,
-            name: product.name,
-            desc: product.desc,
-            price: product.price
-        }
-    } else {
-        return res.status(400).jsend.error({
-            message: "Product not found",
-        });
+    if (!product) {
+      return res.status(400).jsend.error({
+        message: 'Product not found.',
+      });
     }
+  }
+});
 
-    res.jsend.success(data)
-})
+exports.deleteProduct = asyncHandler(async (req, res, next) => {
+  const { product_id } = req.body;
+  const { id, privilegesId } = req.user;
 
-exports.createProduct = asyncHandler(async(req, res, next) => {
-    const {
-        product_name,
-        product_desc,
-        product_price
-    } = req.body
-    const { id, privilegesId } = req.user
+  const findProduct = await DB_ITEMS.findOne({
+    where: {
+      id: product_id,
+    },
+  });
 
-    if(privilegesId == 1){
-        try {
-            const createProd = await DB_ITEMS.create({
-                usersId: id,
-                name: product_name,
-                desc: product_desc,
-                price: product_price
-            })
+  if (privilegesId == 1 && findProduct) {
+    findProduct.destroy();
 
-            res.jsend.success({
-                message: "Product has been created."
-            })
-        } catch (error) {
-            return res.status(400).jsend.error({
-                message: "Failed! Product has not been created.",
-            });
-        }
-    } else {
-        return res.status(400).jsend.error({
-            message: "User is not Admin.",
-        });
+    res.jsend.success({
+      message: 'Product has been deleted.',
+    });
+  } else {
+    if (privilegesId != 1) {
+      return res.status(400).jsend.error({
+        message: 'User is not Admin.',
+      });
     }
-})
-
-exports.updateProduct = asyncHandler(async(req, res, next) => {
-    const {
-        product_id,
-        name,
-        desc,
-        price
-    } = req.body
-    const { id, privilegesId } = req.user
-
-    const now = new Date()
-
-    const product = await DB_ITEMS.findOne({
-        where:{
-            id: product_id
-        }
-    })
-
-    if(privilegesId == 1 && product){
-        product.name = name,
-        product.desc = desc,
-        product.price = price
-        product.updatedAt = now
-        await product.save()
-
-        res.jsend.success({
-            message: "Product has been updated."
-        })
-    } else {
-        if(privilegesId != 1){
-            return res.status(400).jsend.error({
-                message: "User is not Admin.",
-            });   
-        }
-        if(!product){
-            return res.status(400).jsend.error({
-                message: "Product not found.",
-            });
-        }
+    if (!findProduct) {
+      return res.status(400).jsend.error({
+        message: 'Product not found.',
+      });
     }
-})
+  }
+});
 
-exports.deleteProduct = asyncHandler(async(req, res, next) => {
-    const {
-        product_id
-    } = req.body
-    const { id, privilegesId } = req.user
+exports.productBuy = asyncHandler(async (req, res, next) => {
+  const { product_id, quantity, message } = req.body;
+  const { id } = req.user;
 
-    const findProduct = await DB_ITEMS.findOne({
-        where:{
-            id: product_id
-        }
-    })
+  const product = await DB_ITEMS.findOne({
+    where: {
+      id: product_id,
+    },
+  });
 
-    if(privilegesId == 1 && findProduct){
-        findProduct.destroy()
+  if (product) {
+    try {
+      const createPurchasing = await DB_PURCHASINGS.create({
+        codeTransaction: `CODETR${moment().format('YYYYMMDD')}${
+          product.id
+        }${id}RILIV`,
+        buyerId: id,
+        itemsId: product.id,
+        itemsName: product.name,
+        itemsDesc: product.desc,
+        price: product.price * quantity,
+        message: message,
+      });
 
-        res.jsend.success({
-            message: "Product has been deleted."
-        })
-    } else {
-        if(privilegesId != 1){
-            return res.status(400).jsend.error({
-                message: "User is not Admin.",
-            });   
-        }
-        if(!findProduct){
-            return res.status(400).jsend.error({
-                message: "Product not found.",
-            });
-        }
+      res.jsend.success({
+        message: 'Product has been purchased.',
+      });
+    } catch (error) {
+      return res.status(400).jsend.error({
+        message: 'Failed! Product not been purchased.',
+      });
     }
-})
+  } else {
+    return res.status(400).jsend.error({
+      message: 'Product not found.',
+    });
+  }
+});
 
-exports.productBuy = asyncHandler(async(req, res, next) => {
-    const {
-        product_id,
-        message
-    } = req.body
-    const { id } = req.user
+exports.listTransactions = asyncHandler(async (req, res, next) => {
+  const { id, privilegesId } = req.user;
 
-    const product = await DB_ITEMS.findOne({
-        where:{
-            id: product_id
-        }
-    })
+  let query = {
+    where: {},
+  };
 
-    if(product){
-        try {
-            const createPurchasing = await DB_PURCHASINGS.create({
-                codeTransaction: `CODETR${moment().format('YYYYMMDD')}${product.id}${id}RILIV`,
-                buyerId: id,
-                itemsId: product.id,
-                itemsName: product.name,
-                itemsDesc: product.desc,
-                price: product.price,
-                message: message
-            })
+  if (privilegesId != 1) {
+    query.where['buyerId'] = id;
+  }
 
-            res.jsend.success({
-                message: "Product has been purchased."
-            })
-        } catch (error) {
-            return res.status(400).jsend.error({
-                message: "Failed! Product not been purchased.",
-            });
-        }
-    } else {
-        return res.status(400).jsend.error({
-            message: "Product not found.",
-        });
-    }
-})
+  const transactions = await DB_PURCHASINGS.findAll({
+    ...query,
+  });
 
-exports.listTransactions = asyncHandler(async(req, res, next) => {
-    const { id, privilegesId } = req.user
+  let data = {
+    sumOfTransaction: 0,
+    list: [],
+  };
+  if (transactions) {
+    data.sumOfTransaction = transactions.length;
+    data.list = transactions.map((transaction) => {
+      return {
+        id: transaction.id,
+        codeTransaction: transaction.codeTransaction,
+        buyerId: transaction.buyerId,
+        productId: transaction.itemsId,
+        name: transaction.itemsName,
+        desc: transaction.itemsDesc,
+        price: transaction.price,
+        message: transaction.message ? transaction.message : null,
+      };
+    });
+  }
 
-    let query = {
-        where:{}
-    }
+  res.jsend.success(data);
+});
 
-    if(privilegesId != 1){
-        query.where["buyerId"] = id
-    }
-
-    const transactions = await DB_PURCHASINGS.findAll({
-        ...query
-    })
-
-    let data = {
-        sumOfTransaction: 0,
-        list: []
-    }
-    if(transactions){
-        data.sumOfTransaction = transactions.length
-        data.list = transactions.map(transaction => {
-            return {
-                id: transaction.id,
-                codeTransaction: transaction.codeTransaction,
-                buyerId: transaction.buyerId,
-                productId: transaction.itemsId,
-                name: transaction.itemsName,
-                desc: transaction.itemsDesc,
-                price: transaction.price,
-                message: (transaction.message) ? transaction.message : null
-            }
-        })
-    }
-
-    res.jsend.success(data)
-})
+exports.productsBuy = asyncHandler(async (req, res, next) => {});
--- server/routes/items.js
+++ server/routes/items.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const { authenticate } = require("../middleware/auth")
+const express = require('express');
+const { authenticate } = require('../middleware/auth');
 const router = express.Router();
 const {
-    getAllProducts,
-    getProduct,
-    createProduct,
-    updateProduct,
-    deleteProduct,
-    productBuy,
-    listTransactions
-} = require("../controllers/items");
+  getAllProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  productBuy,
+  listTransactions,
+  productsBuy,
+} = require('../controllers/items');
 
-router.get("/products", authenticate, getAllProducts);
-router.get("/product", authenticate, getProduct);
-router.post("/product", authenticate, createProduct);
-router.put("/product", authenticate, updateProduct);
-router.delete("/product", authenticate, deleteProduct);
+router.get('/products', authenticate, getAllProducts);
+router.get('/product', authenticate, getProduct);
+router.post('/product', authenticate, createProduct);
+router.put('/product', authenticate, updateProduct);
+router.delete('/product', authenticate, deleteProduct);
 
-router.post("/product-buy", authenticate, productBuy);
-router.get("/product-transactions", authenticate, listTransactions);
+router.post('/product-buy', authenticate, productBuy);
+router.post('/products-buy', authenticate, productsBuy);
+router.get('/product-transactions', authenticate, listTransactions);
 
 module.exports = router;
